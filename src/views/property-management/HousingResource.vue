b9c564a4a34c2301eb055c1d13723aa903afd552
Violet_Ice紫冰 <violetice@aliyun.com>
2019-10-28 16:42:05 +0800

    Add 楼宇房源占位

---
+++ src/views/property-management/HousingResource.vue
@@ -0,0 +1,19 @@
+<template>
+  <a-card :bordered="false">
+    <div class="property-management-housing-resource">
+      房源列表
+    </div>
+  </a-card>
+</template>
+
+<script>
+  export default {
+    name: 'HousingResource'
+  }
+</script>
+
+<style lang="less">
+  .property-management-housing-resource {
+
+  }
+</style>
