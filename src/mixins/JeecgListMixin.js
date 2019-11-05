538a75466f68ececc6ac87794dabec1337b74c99
Violet_Ice紫冰 <violetice@aliyun.com>
2019-11-05 21:38:06 +0800

    Fix 兼容原来的底层处理，防止大面积错误

--- src/mixins/JeecgListMixin.js
+++ src/mixins/JeecgListMixin.js
@@ -136,16 +136,18 @@ export const JeecgListMixin = {
         this.$message.error("请设置url.deleteBatch属性!")
         return
       }
-      if(!this.deleteKey){
-        this.$message.error("请设置deleteKey，删除的主键!")
-        return
-      }
       if (this.selectionRows.length <= 0) {
         this.$message.warning('请选择一条记录！');
       } else {
+        if(!this.deleteKey){
+          if(!this.selectionRows[0].hasOwnProperty('id')){
+            this.$message.error("请设置deleteKey，删除的主键!")
+            return
+          }
+        }
         var ids = "";
         for (var a = 0; a < this.selectionRows.length; a++) {
-          ids += this.selectionRows[a][this.deleteKey] + ",";
+          ids += this.selectionRows[a][this.deleteKey ? this.deleteKey : 'id'] + ",";
         }
         var that = this;
         this.$confirm({
@@ -170,12 +172,17 @@ export const JeecgListMixin = {
         this.$message.error("请设置url.delete属性!")
         return
       }
+      if(typeof data !== 'object'){
+        data = {id: data}
+      }
       if(!this.deleteKey){
-        this.$message.error("请设置deleteKey，删除的主键!")
-        return
+        if(!data.hasOwnProperty('id')){
+          this.$message.error("请设置deleteKey，删除的主键!")
+          return
+        }
       }
       var that = this;
-      deleteAction(that.url.delete, {id: data[this.deleteKey]}).then((res) => {
+      deleteAction(that.url.delete, {id: data[this.deleteKey ? this.deleteKey : 'id']}).then((res) => {
         if (res.success) {
           that.$message.success(res.message);
           that.loadData();
