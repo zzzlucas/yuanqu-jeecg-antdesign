1661780ca22135918bf79009ff40e29775751244
Violet_Ice紫冰 <violetice@aliyun.com>
2019-11-05 19:28:13 +0800

    Fix 园区管理：表单基础完成

--- src/views/industrial-parks/components/ParksAddForm.vue
+++ src/views/industrial-parks/components/ParksAddForm.vue
@@ -13,16 +13,70 @@
           <a-form-item label="园区名称" :label-col="formItem.label" :wrapper-col="formItem.value">
             <a-input v-decorator="['park_name', {rules: [{required: true, message: '请输入园区名称'}]}]"></a-input>
           </a-form-item>
+          <a-form-item label="园区简介" :label-col="formItem.label" :wrapper-col="formItem.value">
+            <j-editor v-decorator="['content']"></j-editor>
+          </a-form-item>
+          <a-form-item label="优惠政策" :label-col="formItem.label" :wrapper-col="formItem.value">
+            <j-editor v-decorator="['policy']"></j-editor>
+          </a-form-item>
         </a-col>
         <a-col span="12">
           <a-form-item label="总建筑面积" :label-col="formItem.label" :wrapper-col="formItem.value">
-            <a-input v-decorator="['total_buliding_area']"></a-input>
+            <a-input v-decorator="['total_buliding_area']">
+              <span slot="addonAfter">㎡</span>
+            </a-input>
           </a-form-item>
           <a-form-item label="工位总数" :label-col="formItem.label" :wrapper-col="formItem.value">
             <a-input v-decorator="['total_workstation']"></a-input>
           </a-form-item>
           <a-form-item label="工位收费标准" :label-col="formItem.label" :wrapper-col="formItem.value">
-            <a-input v-decorator="['fee_scale']"></a-input>
+            <span>
+              <a-input v-decorator="['fee_scale']">
+                <a-select slot="addonAfter" style="width: 100px;" v-decorator="['_fee_scale_end', {initialValue: '1'}]">
+                  <a-select-option value="1">元/天</a-select-option>
+                  <a-select-option value="2">元/月</a-select-option>
+                  <a-select-option value="3">元/年</a-select-option>
+                </a-select>
+              </a-input>
+            </span>
+          </a-form-item>
+          <a-form-item label="独立空间总数" :label-col="formItem.label" :wrapper-col="formItem.value">
+            <a-input v-decorator="['total_room']"></a-input>
+          </a-form-item>
+          <a-form-item label="独立空间收费标准" :label-col="formItem.label" :wrapper-col="formItem.value">
+            <span>
+              <a-input v-decorator="['fee_scale_room_unit']">
+                <a-select slot="addonAfter" style="width: 100px" v-decorator="['_fee_scale_room_unit_end', {initialValue: '1'}]">
+                  <a-select-option value="1">元/㎡/天</a-select-option>
+                  <a-select-option value="2">元/㎡/月</a-select-option>
+                  <a-select-option value="3">元/㎡/年</a-select-option>
+                </a-select>
+              </a-input>
+            </span>
+          </a-form-item>
+          <a-form-item label="地址" :label-col="formItem.label" :wrapper-col="formItem.value">
+            <a-input v-decorator="['address']"></a-input>
+          </a-form-item>
+          <a-form-item label="经度" :label-col="formItem.label" :wrapper-col="formItem.value">
+            <a-input v-decorator="['lng']"></a-input>
+          </a-form-item>
+          <a-form-item label="纬度" :label-col="formItem.label" :wrapper-col="formItem.value">
+            <a-input v-decorator="['lat']"></a-input>
+          </a-form-item>
+          <a-form-item label="得房率" :label-col="formItem.label" :wrapper-col="formItem.value">
+            <a-input v-decorator="['room_rate']">
+              <span slot="addonAfter">%</span>
+            </a-input>
+          </a-form-item>
+          <a-form-item label="联系电话" :label-col="formItem.label" :wrapper-col="formItem.value">
+            <a-input v-decorator="['telephone']"></a-input>
+          </a-form-item>
+          <a-form-item label="具备设备" :label-col="formItem.label" :wrapper-col="formItem.value">
+            <a-select mode="tags" v-decorator="['device_group_id']">
+              <a-select-option value="1">桌子</a-select-option>
+              <a-select-option value="2">椅子</a-select-option>
+              <a-select-option value="3">电脑</a-select-option>
+            </a-select>
           </a-form-item>
         </a-col>
       </a-row>
@@ -36,8 +90,10 @@
 </template>
 
 <script>
+  import JEditor from '@comp/jeecg/JEditor'
   export default {
     name: 'ParksAddForm',
+    components: { JEditor },
     props: {
       show: {
         type: Boolean,
@@ -64,9 +120,9 @@
       handleSubmit() {
         this.form.validateFieldsAndScroll(this.onSubmit.bind(this))
       },
-      onSubmit(err) {
+      onSubmit(err, form) {
         if (err === null) {
-
+          console.log(form)
         }
       }
     }
