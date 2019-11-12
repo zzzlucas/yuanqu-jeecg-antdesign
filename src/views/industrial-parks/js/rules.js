395a4de62171d7b94a5cdc39de99ca2681126a5c
Violet_Ice紫冰 <violetice@aliyun.com>
2019-11-13 01:42:56 +0800

    Fix 常用正则独立配置

--- src/views/industrial-parks/js/rules.js
+++ src/views/industrial-parks/js/rules.js
@@ -1,3 +1,5 @@
+import { phone } from '@/config/regex'
+
 /**
  * 经纬度校验
  * @param type
@@ -80,6 +82,6 @@ export default {
     }
   ],
   telephone: [
-    {pattern: /^1(3\d|4[5-7]|5[0-35-9]|7[0135-8]|8\d|9[89])\d{8}$/, message: '请输入正确的手机号'}
+    {pattern: phone, message: '请输入正确的手机号'}
   ]
 }
