ed02dbcda5b8ef99cdbdb2ee7001ab21fcc07fc9
Violet_Ice紫冰 <violetice@aliyun.com>
2019-11-15 16:39:02 +0800

    Fix Util 表单promise

--- src/utils/util.js
+++ src/utils/util.js
@@ -8,7 +8,7 @@ export function timeFix() {
 
 export function welcome() {
   const arr = ['休息一会儿吧', '准备吃什么呢?', '要不要打一把 DOTA', '我猜你可能累了']
-  let index = Math.floor((Math.random()*arr.length))
+  let index = Math.floor((Math.random() * arr.length))
   return arr[index]
 }
 
@@ -29,16 +29,16 @@ export function triggerWindowResizeEvent() {
  */
 export function filterObj(obj) {
   if (!(typeof obj == 'object')) {
-    return;
+    return
   }
 
-  for ( var key in obj) {
+  for (var key in obj) {
     if (obj.hasOwnProperty(key)
       && (obj[key] == null || obj[key] == undefined || obj[key] === '')) {
-      delete obj[key];
+      delete obj[key]
     }
   }
-  return obj;
+  return obj
 }
 
 /**
@@ -48,10 +48,10 @@ export function filterObj(obj) {
  * @returns {*}
  */
 export function formatDate(value, fmt) {
-  var regPos = /^\d+(\.\d+)?$/;
-  if(regPos.test(value)){
+  var regPos = /^\d+(\.\d+)?$/
+  if (regPos.test(value)) {
     //如果是数字
-    let getDate = new Date(value);
+    let getDate = new Date(value)
     let o = {
       'M+': getDate.getMonth() + 1,
       'd+': getDate.getDate(),
@@ -60,7 +60,7 @@ export function formatDate(value, fmt) {
       's+': getDate.getSeconds(),
       'q+': Math.floor((getDate.getMonth() + 3) / 3),
       'S': getDate.getMilliseconds()
-    };
+    }
     if (/(y+)/.test(fmt)) {
       fmt = fmt.replace(RegExp.$1, (getDate.getFullYear() + '').substr(4 - RegExp.$1.length))
     }
@@ -69,79 +69,79 @@ export function formatDate(value, fmt) {
         fmt = fmt.replace(RegExp.$1, (RegExp.$1.length === 1) ? (o[k]) : (('00' + o[k]).substr(('' + o[k]).length)))
       }
     }
-    return fmt;
-  }else{
+    return fmt
+  } else {
     //TODO
-    value = value.trim();
-    return value.substr(0,fmt.length);
+    value = value.trim()
+    return value.substr(0, fmt.length)
   }
 }
 
 // 生成首页路由
 export function generateIndexRouter(data) {
-let indexRouter = [{
-          path: '/',
-          name: 'dashboard',
-          //component: () => import('@/components/layouts/BasicLayout'),
-          component: resolve => require(['@/components/layouts/TabLayout'], resolve),
-          meta: { title: '首页' },
-          redirect: '/dashboard/analysis',
-          children: [
-            ...generateChildRouters(data)
-          ]
-        },{
-          "path": "*", "redirect": "/404", "hidden": true
-        }]
-  return indexRouter;
+  let indexRouter = [{
+    path: '/',
+    name: 'dashboard',
+    //component: () => import('@/components/layouts/BasicLayout'),
+    component: resolve => require(['@/components/layouts/TabLayout'], resolve),
+    meta: { title: '首页' },
+    redirect: '/dashboard/analysis',
+    children: [
+      ...generateChildRouters(data)
+    ]
+  }, {
+    'path': '*', 'redirect': '/404', 'hidden': true
+  }]
+  return indexRouter
 }
 
 // 生成嵌套路由（子路由）
 
-function  generateChildRouters (data) {
-  const routers = [];
+function generateChildRouters(data) {
+  const routers = []
   for (var item of data) {
-    let component = "";
-    if(item.component.indexOf("layouts")>=0){
-       component = "components/"+item.component;
-    }else{
-       component = "views/"+item.component;
+    let component = ''
+    if (item.component.indexOf('layouts') >= 0) {
+      component = 'components/' + item.component
+    } else {
+      component = 'views/' + item.component
     }
 
     // eslint-disable-next-line
-    let URL = (item.meta.url|| '').replace(/{{([^}}]+)?}}/g, (s1, s2) => eval(s2)) // URL支持{{ window.xxx }}占位符变量
+    let URL = (item.meta.url || '').replace(/{{([^}}]+)?}}/g, (s1, s2) => eval(s2)) // URL支持{{ window.xxx }}占位符变量
     if (isURL(URL)) {
-      item.meta.url = URL;
+      item.meta.url = URL
     }
 
-    let menu =  {
+    let menu = {
       path: item.path,
       name: item.name,
-      redirect:item.redirect,
-      component: resolve => require(['@/' + component+'.vue'], resolve),
-      hidden:item.hidden,
+      redirect: item.redirect,
+      component: resolve => require(['@/' + component + '.vue'], resolve),
+      hidden: item.hidden,
       //component:()=> import(`@/views/${item.component}.vue`),
       meta: {
-        title:item.meta.title ,
+        title: item.meta.title,
         icon: item.meta.icon,
-        url:item.meta.url ,
-        permissionList:item.meta.permissionList,
-        keepAlive:item.meta.keepAlive
+        url: item.meta.url,
+        permissionList: item.meta.permissionList,
+        keepAlive: item.meta.keepAlive
       }
     }
-    if(item.alwaysShow){
-      menu.alwaysShow = true;
-      menu.redirect = menu.path;
+    if (item.alwaysShow) {
+      menu.alwaysShow = true
+      menu.redirect = menu.path
     }
     if (item.children && item.children.length > 0) {
-      menu.children = [...generateChildRouters( item.children)];
+      menu.children = [...generateChildRouters(item.children)]
     }
     //--update-begin----author:scott---date:20190320------for:根据后台菜单配置，判断是否路由菜单字段，动态选择是否生成路由（为了支持参数URL菜单）------
     //判断是否生成路由
-    if(item.route && item.route === '0'){
+    if (item.route && item.route === '0') {
       //console.log(' 不生成路由 item.route：  '+item.route);
       //console.log(' 不生成路由 item.path：  '+item.path);
-    }else{
-      routers.push(menu);
+    } else {
+      routers.push(menu)
     }
     //--update-end----author:scott---date:20190320------for:根据后台菜单配置，判断是否路由菜单字段，动态选择是否生成路由（为了支持参数URL菜单）------
   }
@@ -174,7 +174,7 @@ export function randomNumber() {
   }
   if (arguments.length === 1) {
     let [length] = arguments
-  // 生成指定长度的随机数字，首位一定不是 0
+    // 生成指定长度的随机数字，首位一定不是 0
     let nums = [...Array(length).keys()].map((i) => (i > 0 ? random(0, 9) : random(1, 9)))
     return parseInt(nums.join(''))
   } else if (arguments.length >= 2) {
@@ -216,10 +216,10 @@ export function randomUUID() {
  * @param string
  * @returns {*}
  */
-export function underLine2CamelCase(string){
-  return string.replace( /_([a-z])/g, function( all, letter ) {
-    return letter.toUpperCase();
-  });
+export function underLine2CamelCase(string) {
+  return string.replace(/_([a-z])/g, function(all, letter) {
+    return letter.toUpperCase()
+  })
 }
 
 /**
@@ -227,11 +227,11 @@ export function underLine2CamelCase(string){
  * @param bpmStatus
  * @returns {*}
  */
-export function showDealBtn(bpmStatus){
-  if(bpmStatus!="1"&&bpmStatus!="3"&&bpmStatus!="4"){
-    return true;
+export function showDealBtn(bpmStatus) {
+  if (bpmStatus != '1' && bpmStatus != '3' && bpmStatus != '4') {
+    return true
   }
-  return false;
+  return false
 }
 
 /**
@@ -241,7 +241,7 @@ export function showDealBtn(bpmStatus){
  */
 export function cssExpand(css, id) {
   let style = document.createElement('style')
-  style.type = "text/css"
+  style.type = 'text/css'
   style.innerHTML = `@charset "UTF-8"; ${css}`
   // 清除旧样式
   if (id) {
@@ -251,4 +251,21 @@ export function cssExpand(css, id) {
   }
   // 应用新样式
   document.head.appendChild(style)
+}
+
+/**
+ * 表单promise
+ * @param form
+ * @returns {Promise<unknown>}
+ */
+export function promiseForm(form) {
+  return new Promise((resolve, reject) => {
+    form.validateFields((err, fields) => {
+      if (!err) {
+        resolve(fields)
+      } else {
+        reject(new Error('没有通过表单校验'))
+      }
+    })
+  })
 }
