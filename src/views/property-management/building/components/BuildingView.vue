af2153e77676d3084ab3d68d630ca717b9a40bc0
Violet_ice紫冰 <violetice@aliyun.com>
2019-11-19 15:10:39 +0800

    Fix 楼宇房源：分离非主流 less

--- src/views/property-management/building/components/BuildingView.vue
+++ src/views/property-management/building/components/BuildingView.vue
@@ -2,11 +2,12 @@
   <div class="yq-building-view">
     <!-- 区块显示 -->
     <a-row class="block-row">
-      <a-col span="8" class="block-item">1</a-col>
-      <a-col span="8" class="block-item">2</a-col>
-      <a-col span="8" class="block-item">3</a-col>
-      <a-col span="8" class="block-item">4</a-col>
-      <a-col span="8" class="block-item">5</a-col>
+      <a-col span="8" class="block-item">
+        <header class="block">A区域</header>
+      </a-col>
+      <a-col span="8" class="block-item">
+        <header class="block">B区域</header>
+      </a-col>
     </a-row>
   </div>
 </template>
@@ -18,29 +19,14 @@
 </script>
 
 <style lang="less">
+  @import "../less/function";
+
   .yq-building-view {
     .block-row {
       .block-item {
         background-color: #00A0E9;
 
-        &:nth-of-type(3n + 1) {
-          margin-right: 4px;
-          width: calc((100% / 24) * 8 - 4px);
-        }
-
-        &:nth-of-type(3n + 2) {
-          margin: 0 4px;
-          width: calc((100% / 24) * 8 - 8px);
-        }
-
-        &:nth-of-type(3n) {
-          margin-left: 4px;
-          width: calc((100% / 24) * 8 - 4px);
-        }
-
-        &:nth-of-type(n + 4) {
-          margin-top: 8px;
-        }
+        .buildBlock();
       }
     }
   }
