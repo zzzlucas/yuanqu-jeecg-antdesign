2f59d979c310c98f6584733437387fb9d43c1fd2
Violet_ice紫冰 <violetice@aliyun.com>
2019-11-19 15:03:18 +0800

    Fix 楼宇房源：区块显示的布局

--- src/views/property-management/building/Building.vue
+++ src/views/property-management/building/Building.vue
@@ -2,21 +2,7 @@
   <a-row :gutter="8">
     <a-col span="4">
       <a-card>
-        <a-tree show-line @select="onTreeSelect">
-          <a-tree-node key="1">
-            <span slot="title">A区</span>
-            <a-tree-node key="2" title="A楼"></a-tree-node>
-          </a-tree-node>
-          <a-tree-node key="3">
-            <span slot="title">B区</span>
-            <a-tree-node key="4" title="B楼">
-              <a-tree-node key="5">
-                <span slot="title">1层</span>
-                <a-tree-node key="6" title="茅房"></a-tree-node>
-              </a-tree-node>
-            </a-tree-node>
-          </a-tree-node>
-        </a-tree>
+        <building-tree></building-tree>
       </a-card>
     </a-col>
     <a-col span="20">
@@ -27,19 +13,20 @@
           <a-button class="margin-left">新建楼层</a-button>
           <a-button class="margin-left">新建房间</a-button>
         </template>
+        <building-view></building-view>
       </a-card>
     </a-col>
   </a-row>
 </template>
 
 <script>
+  import BuildingTree from './components/BuildingTree'
+  import BuildingView from './components/BuildingView'
+
   export default {
     name: 'Building',
-    methods: {
-      onTreeSelect(keys, info) {
-        console.log(keys, info)
-      }
-    }
+    components: { BuildingView, BuildingTree },
+    methods: {}
   }
 </script>
 
