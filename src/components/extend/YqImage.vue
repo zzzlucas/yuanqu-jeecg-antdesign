777906333afe2e540bc8bdde0b82d9becfd28753
Violet_ice紫冰 <violetice@aliyun.com>
2019-11-19 16:16:41 +0800

    Fix 公共的图片展示组件

---
+++ src/components/extend/YqImage.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="yq-image" @click="onClick">
+    <div class="image-text" v-if="error">
+      <a-icon type="picture" :style="{fontSize: size + 'px'}"/>
+    </div>
+    <img class="this-image" v-else :src="src" alt="图片" @error="imageErr">
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'YqImage',
+    props: {
+      src: {
+        type: String,
+        default: ''
+      },
+      size: {
+        type: [Number, String],
+        default: 16
+      }
+    },
+    data () {
+      return {
+        error: false
+      }
+    },
+    methods: {
+      imageErr () {
+        this.error = true
+      },
+      onClick (e) {
+        this.$emit('click', e)
+      }
+    },
+    watch: {
+      src: function () {
+        this.error = false
+      }
+    }
+  }
+</script>
+
+<style lang="less">
+  .yq-image {
+    width: 100%;
+    height: 100%;
+    display: block;
+    background-color: #f2f2f2;
+    overflow: hidden;
+
+    .image-text {
+      width: 100%;
+      height: 100%;
+      color: #6e6e6e;
+      text-align: center;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+
+    .this-image {
+      display: block;
+      width: 100%;
+      height: 100%;
+      object-fit: fill;
+    }
+  }
+</style>
