f42f272daf2422762a2481123074d5a957a9192c
Violet_ice紫冰 <violetice@aliyun.com>
2019-11-19 15:19:22 +0800

    Fix 楼宇房源：两列式，二级间距

--- src/views/property-management/building/components/BuildingView.vue
+++ src/views/property-management/building/components/BuildingView.vue
@@ -2,11 +2,17 @@
   <div class="yq-building-view">
     <!-- 区块显示 -->
     <a-row class="block-row">
-      <a-col span="8" class="block-item">
+      <a-col span="12" class="block-item">
         <header class="block">A区域</header>
       </a-col>
-      <a-col span="8" class="block-item">
+      <a-col span="12" class="block-item">
         <header class="block">B区域</header>
+      </a-col>
+      <a-col span="12" class="block-item">
+        <header class="block">C区域</header>
+      </a-col>
+      <a-col span="12" class="block-item">
+        <header class="block">D区域</header>
       </a-col>
     </a-row>
   </div>
@@ -24,7 +30,12 @@
   .yq-building-view {
     .block-row {
       .block-item {
-        background-color: #00A0E9;
+        header.block {
+          width: 100%;
+          padding: 10px;
+          background-color: #d9d9d9;
+          font-size: 16px;
+        }
 
         .buildBlock();
       }
