55e8dd55397bcfd69268ca7c8a0b6847829edb88
FF <496447115@qq.com>
2019-11-24 15:38:07 +0800

    use window._CONFIG['domianURL'] as basePath instead of hard code config value

--- src/utils/request.js
+++ src/utils/request.js
@@ -7,7 +7,7 @@ import { ACCESS_TOKEN } from "@/store/mutation-types"
 
 // 创建 axios 实例
 const service = axios.create({
-  baseURL: '/jeecg-boot', // 开发环境去 vue.config.js 指定请求接口，这里会影响到 build
+  baseURL: window._CONFIG['domianURL'], // 开发环境去 vue.config.js 指定请求接口，这里会影响到 build
   timeout: 6000 // 请求超时时间
 })
 
