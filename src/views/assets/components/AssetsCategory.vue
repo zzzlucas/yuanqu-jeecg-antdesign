d0b26895888e17eff1702c59fe7f70f56026b911
FF <496447115@qq.com>
2019-11-24 16:03:34 +0800

    remove test nodes

--- src/views/assets/components/AssetsCategory.vue
+++ src/views/assets/components/AssetsCategory.vue
@@ -15,46 +15,7 @@
   export default {
     data() {
       return {
-        nodes: [
-          {
-            key: '0-0',
-            title: '易耗品类',
-            children: [
-              {
-                key: '0-0-0',
-                title: '日常生活办公',
-              },
-              {
-                key: '0-0-1',
-                title: '生活类',
-              },
-              {
-                key: '0-0-2',
-                title: '办公类',
-              },
-            ],
-          },
-          {
-            key: '0-1',
-            title: '固定资产',
-            children: [
-              {
-                key: '0-1-0',
-                title: '运输车辆',
-              },
-              {
-                key: '0-1-1',
-                title: 'IT设备类',
-                children: [
-                  {
-                    key: '0-1-1-0',
-                    title: '笔记本',
-                  },
-                ]
-              },
-            ],
-          }
-        ],
+        nodes: [],
         list: [],
       }
     },
