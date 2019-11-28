4cb62c0e29a23dfc0c904a9d6288e21e878aafdf
FF <496447115@qq.com>
2019-11-28 23:09:52 +0800

    dev on event edit form

--- src/views/property-management/components/MeetingWeekView.vue
+++ src/views/property-management/components/MeetingWeekView.vue
@@ -4,18 +4,26 @@
       ref="calendar"
       :events="events"
       :config="config" />
+    <!-- Add/Edit form -->
+    <meeting-event-edit-form
+      ref="modalForm"
+      @submit="handleEditSubmit" />
   </div>
 </template>
 
 <script>
-  import Mixin from '../mixin/calendar'
   import 'fullcalendar-scheduler';
   import 'fullcalendar-scheduler/dist/scheduler.min.css';
+  import MeetingEventEditForm from './MeetingEventEditForm'
+  import Mixin from '../mixin/calendar'
 
   export default {
     mixins: [
       Mixin,
     ],
+    components: {
+      MeetingEventEditForm,
+    },
     data() {
       return {
         config: {
@@ -28,8 +36,23 @@
           },
           defaultView: 'timelineWeek',
           resourceLabelText: "会议室",
+          dayClick: (date, jsEvent, view) => {
+            const formatDate = date.format()
+            this.handleAddEvent(formatDate)
+          }
         }
       }
     },
+    methods: {
+      handleAddEvent(date) {
+        this.$refs.modalForm.add({ begDate: date });
+        this.$refs.modalForm.title = "添加";
+        this.$refs.modalForm.disableSubmit = false;
+      },
+      // Add/Edit
+      async handleEditSubmit() {
+        this.loadData()
+      },
+    }
   }
 </script>
